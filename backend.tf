a9e347de94b93eb47690235fbdbe98fcdff4859a
Dananjaya <163394188+Dananjayagm@users.noreply.github.com>
2024-05-19 21:41:20 +0530

    removed key location

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-s3-bucket-name"
-    key            = "path/to/your/terraform.tfstate"
+    bucket         = "terraform-statefile"
+    key            = "**********/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
   }

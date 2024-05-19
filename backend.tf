c9f3430bb3d730d095e3b04762dbeb31347a18cf
Dananjaya <163394188+Dananjayagm@users.noreply.github.com>
2024-05-19 19:47:32 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "ecs-cluster/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
